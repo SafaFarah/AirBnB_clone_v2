000efd2cdb1729fafd96ccb25a006d1bc62f48d8
SafaFarah <122905189+SafaFarah@users.noreply.github.com>
2024-01-17 15:15:55 +0200

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- a new user usernam ='hbnb_test' , host = 'localhost', password = 'hbnb_test_pwd'.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- hbnb_test should have all privileges on the database hbnb_test_db.
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 -- hbnb_test should have SELECT privilege on the database performance_schema.
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
