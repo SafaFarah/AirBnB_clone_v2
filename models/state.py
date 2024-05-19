24b0026a78cb7845848cd6039bdb1d1f347d4db1
SafaFarah <kangjisook13@gmail.com>
2024-05-19 19:46:13 +0300

    task 7 update

--- models/state.py
+++ models/state.py
@@ -6,3 +6,13 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Return the list of City from storage linked to current State"""
+            cities = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
