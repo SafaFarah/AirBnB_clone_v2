b024ca65282604d74e9a61befe25b79c01cbe08b
SafaFarah <kangjisook13@gmail.com>
2024-05-19 19:56:17 +0300

    debug pycodestyle

--- models/state.py
+++ models/state.py
@@ -11,8 +11,5 @@ class State(BaseModel):
         @property
         def cities(self):
             """Return the list of City from storage linked to current State"""
-            cities = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    cities.append(city)
-            return cities
+            return [city for city in models.storage.all(
+                City).values() if city.state_id == self.id]
