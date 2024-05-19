7151be54e0b2e418b4668095415c9c5fc118a859
SafaFarah <kangjisook13@gmail.com>
2024-05-19 19:52:44 +0300

    debug pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -121,5 +121,5 @@ class DBStorage:
             print(E)
 
     def close(self):
-        """call remove() method on the private session attribute (self.__session) """
+        """call remove()`on the private session attribute (self.__session) """
         self.__session.remove()
