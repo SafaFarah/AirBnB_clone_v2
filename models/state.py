01e5d002d48090b666fc2340cbf7bcc643cb8b5d
SafaFarah <kangjisook13@gmail.com>
2024-07-12 19:50:29 +0300

    debug state.py

--- models/state.py
+++ models/state.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import environ
 from models.base_model import BaseModel
 from models.city import City
+from models import storage
 import models
 
 class State(BaseModel):
     """ State class """
     name = ""
 
-    if environ.get('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """Return the list of City from storage linked to current State"""
-            all_cities = storage.all(City)
-            state_cities = [city for city in all_cities.values() if city.state_id == self.id]
-            return state_cities
+    @property
+    def cities(self):
+        """Return the list of City from storage linked to current State"""
+        all_cities = storage.all(City)
+        state_cities = [city for city in all_cities.values() if city.state_id == self.id]
+        return state_cities
